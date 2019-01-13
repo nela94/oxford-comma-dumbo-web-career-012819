39c3cf3014188f83f799c0139d78abeae77cef03
Manuela  Sanchez <m.sanchez947@hotmail.com>
2019-01-13 03:57:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-  array.join
+  if array.length = 1
+    array.join
+  elsif array.length = 2
+    puts array.join(" and ")
+  else
+    array.length >= 3
+    puts array.join(", ")
+  end  
 end
