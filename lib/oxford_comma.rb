9d9e71ba6025a1f5f40d15077bf5150d2cb9618c
Manuela  Sanchez <m.sanchez947@hotmail.com>
2019-01-13 03:58:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,11 @@
+array = []
 def oxford_comma(array)
-  if array.length = 1
+  if array.length == 1
     array.join
   elsif array.length = 2
     puts array.join(" and ")
   else
     array.length >= 3
     puts array.join(", ")
-  end  
+  end
 end
